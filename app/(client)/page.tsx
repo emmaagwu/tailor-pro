86b73d92104616d6ee61087a5a2fe9d9a9cdb5a7
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2025-04-27 02:10:37 +0100

    Update div class in Home component

--- app/(client)/page.tsx
+++ app/(client)/page.tsx
@@ -3,7 +3,7 @@ import HeroSection from '../components/client/hero-section'
 
 const Home = () => {
   return (
-    <div>
+    <div className='mt-13'>
       <HeroSection
         imageUrl='https://ik.imagekit.io/n34nw5zbn/tailor-hero.jpg?updatedAt=1745710802161'
         altText='Elegant tailored clothing collection'
