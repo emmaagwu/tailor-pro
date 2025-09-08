35db194c1cdd735f7d9f9def61637e6d13130bd8
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2025-09-08 09:38:13 +0100

    Update Prisma configuration and add postinstall script for generation

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'prisma']
+  },
 };
 
 export default nextConfig;
